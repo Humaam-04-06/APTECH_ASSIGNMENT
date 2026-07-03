68116e2969191b4ee259aabf76978b4119f73c6e
syedahmed920 <humaamahmed40@gmail.com>
2026-07-03 18:57:34 +0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAVt8Lmtssp3f-J_RVlDMv1dGLXJnigbA4",
+    authDomain: "product-inventory-app-c043e.firebaseapp.com",
+    projectId: "product-inventory-app-c043e",
+    storageBucket: "product-inventory-app-c043e.firebasestorage.app",
+    messagingSenderId: "97153851031",
+    appId: "1:97153851031:web:77d66bfef34620018513d2",
+    measurementId: "G-K8G5C6F1PQ"
   }
 };
