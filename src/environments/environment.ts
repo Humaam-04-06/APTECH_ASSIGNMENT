1bb1b6960bc8f14857e681851599c6ecd8d93b6d
Ahmed <humaamahmed40@gmail.com>
2026-07-19 16:04:04 +0500

    FIx Security Issues

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAVt8Lmtssp3f-J_RVlDMv1dGLXJnigbA4",
-    authDomain: "product-inventory-app-c043e.firebaseapp.com",
-    projectId: "product-inventory-app-c043e",
-    storageBucket: "product-inventory-app-c043e.firebasestorage.app",
-    messagingSenderId: "97153851031",
-    appId: "1:97153851031:web:77d66bfef34620018513d2",
-    measurementId: "G-K8G5C6F1PQ"
+    apiKey: "Use Your API Key Here!",
+    authDomain: "Add Your Domain",
+    projectId: "Add your project Id ",
+    storageBucket: "Add Your Storage Bucket Here ",
+    messagingSenderId: "Enter Your Message Sender ID",
+    appId: "Enter here your APP ID !",
+    measurementId: "Measurement ID Here !"
   }
 };
